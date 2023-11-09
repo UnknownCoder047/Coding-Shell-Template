becd7e8e09b18161a77634688521ac2ed7ef6bcd
Ankit Kumar <ankitkumarj143@gmail.com>
2023-11-09 23:30:05 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+//Scss File
+
+function scssTask() {
+  return src("src/assets/sass/**/*.scss", { sourcemaps: true })
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(dest("src/assets/css", { sourcemaps: "." }))
+    .pipe(browserSync.stream());
+}
+
+//Browser Sync
+
+function browserSyncServe(cb) {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+//Reload
+
+function browserSyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watchTask() {
+  watch(
+    ["*.html", "/src/pages/**/*.html", "/src/assets/js/**/*.js"],
+    browserSyncReload
+  );
+  //.on('change',browserSyncReload);
+  watch(["/src/assets/sass/**/*.scss"], series(scssTask, browserSyncReload));
+}
+
+exports.default = series(scssTask, browserSyncServe, watchTask);
